44525c90bb3ffff3d333b86ee6ba1583db3bd263
Denislav Georgiev <d1xt1r@gmail.com>
2018-06-27 15:37:25 +0300

    Add Guard script

---
+++ Stealth Game/Assets/Guard.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Guard : MonoBehaviour {
+
+    public Transform pathHolder;
+
+    void OnDrawGizmos() {
+        Vector3 startPosition = pathHolder.GetChild(0).position; // startPosition is the position of the first waypoint
+        Vector3 previousPosition = startPosition; // previousPosition is initially equal to the startPosition
+        foreach (Transform  waypoint in pathHolder) { // loop through all waypoints (child objects) in pathHolder 
+            Gizmos.DrawSphere (waypoint.position, .3f); // draw sphere on the center of the position with radius .3f
+            Gizmos.DrawLine(previousPosition, waypoint.position); // draw line from the previous positon to the current waypoint position
+            previousPosition = waypoint.position; // then the previousPosition get set to the current waypoint position so DrawLine will draw again from previousPosition which is now the next waypoint
+        }
+        Gizmos.DrawLine(previousPosition, startPosition); // close the loop - connect the last waypoint to the first waypoint
+    }
+}
