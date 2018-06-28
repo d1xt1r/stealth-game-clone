7faf08615669b042f89b50d79934f88d68bcef0f
Denislav Georgiev <d1xt1r@gmail.com>
2018-06-28 18:19:14 +0300

    Add more code to Guard script related to moving the guard along the waypoints

--- Stealth Game/Assets/Guard.cs
+++ Stealth Game/Assets/Guard.cs
@@ -4,15 +4,36 @@ using UnityEngine;
 
 public class Guard : MonoBehaviour {
 
-    public Transform pathHolder;
+    public float speed = 5f; // speed of the guard
+    public float waitTime = .3f; // how much the guard will stay at each waypoint
 
-    // 2. Make guard moving along the path
+    public Transform pathHolder;
 
     private void Start() {
 
         Vector3[] waypoints = new Vector3[pathHolder.childCount]; // get array of all of the positions of the waypoints in the pathHolder
         for (int i = 0; i < waypoints.Length; i++) { // loop through all indexes in our array
-            waypoints[i] = pathHolder.GetChild(i).position; // each waypoint with index i should be equal to each child (posistion) with index of i  
+            waypoints[i] = pathHolder.GetChild(i).position; // each waypoint with index i shoulget the position of each child with index of i
+        }
+
+        StartCoroutine(FollowPath(waypoints)); // start the coroutine
+    }
+
+    IEnumerator FollowPath(Vector3[] waypoints) { // for path we are passing the waypoints array so we have count of 5.
+        transform.position = waypoints[0]; // guard is positioned at the first waypoint
+
+        int targetWaypointIndex = 1; // variable to keep track of the index of the waypoint we're currently moving towards. We're already at 0 (the first index), so 1 is the second index.
+        Vector3 targetWaypoint = waypoints[targetWaypointIndex]; // the actuall position of the waypoint
+
+        while (true) { // loop forever 
+            transform.position = Vector3.MoveTowards(transform.position, targetWaypoint, speed * Time.deltaTime); // move the guard from its current position to the target(next) waypoint with a maximum distance of speed * time.deltatime
+            if(transform.position == targetWaypoint) { // if we reach the targetWaypoint
+                targetWaypointIndex = (targetWaypointIndex + 1);/* % waypoints.Length;*/ // move onto the next waypoint and after you
+                print("I am at index " + targetWaypointIndex);
+                targetWaypoint = waypoints[targetWaypointIndex]; // targetWaypoint is set to the next waypoint index
+                yield return new WaitForSeconds(waitTime); // when the guard reach the next waypoint we want to wait for .3 seconds
+            }
+            yield return null; // "wait until the next frame" - yeld for one frame between each iteration of the while loop
         }
     }
 
