8e28b01da3623cabd0282ed940176a5dfe0f22b4
Denislav Georgiev <d1xt1r@gmail.com>
2018-07-02 11:01:01 +0300

    Add more code to Guard script

--- Stealth Game/Assets/Guard.cs
+++ Stealth Game/Assets/Guard.cs
@@ -6,35 +6,56 @@ public class Guard : MonoBehaviour {
 
     public float speed = 5f; // speed of the guard
     public float waitTime = .3f; // how much the guard will stay at each waypoint
+    public float turnSpeed = 90f; // 90 degrees per second - the turn speed of the guard
 
     public Transform pathHolder;
 
     private void Start() {
 
+        // 2.1 Make guard follow the path
+
         Vector3[] waypoints = new Vector3[pathHolder.childCount]; // get array of all of the positions of the waypoints in the pathHolder
         for (int i = 0; i < waypoints.Length; i++) { // loop through all indexes in our array
-            waypoints[i] = pathHolder.GetChild(i).position; // each waypoint with index i shoulget the position of each child with index of i
+            waypoints[i] = pathHolder.GetChild(i).position; // each waypoint with index i should get the position of each child with index of i
+            waypoints[i] = new Vector3(waypoints[i].x, transform.position.y, waypoints[i].z); // make waypoints to be positioned at the Y axis of the guard.
         }
 
         StartCoroutine(FollowPath(waypoints)); // start the coroutine
     }
+
+    // 2.1 Make guard follow the path
 
     IEnumerator FollowPath(Vector3[] waypoints) { // for path we are passing the waypoints array so we have count of 5.
         transform.position = waypoints[0]; // guard is positioned at the first waypoint
 
         int targetWaypointIndex = 1; // variable to keep track of the index of the waypoint we're currently moving towards. We're already at 0 (the first index), so 1 is the second index.
         Vector3 targetWaypoint = waypoints[targetWaypointIndex]; // the actuall position of the waypoint
+        transform.LookAt(targetWaypoint); // make the guard initally faces the target waypoint
 
         while (true) { // loop forever 
             transform.position = Vector3.MoveTowards(transform.position, targetWaypoint, speed * Time.deltaTime); // move the guard from its current position to the target(next) waypoint with a maximum distance of speed * time.deltatime            
             if (transform.position == targetWaypoint) { // if we reach the targetWaypoint
                 targetWaypointIndex = (targetWaypointIndex + 1) % waypoints.Length;// increment targetWaypointIndex by one, and after the guard reach targetWaypointIndex 5 go to 0
-                print("I am at index " + targetWaypointIndex);
+                //print("I am at index " + targetWaypointIndex);
                 targetWaypoint = waypoints[targetWaypointIndex]; // targetWaypoint is set to the next waypoint index
                 yield return new WaitForSeconds(waitTime); // when the guard reach the next waypoint we want to wait for .3 seconds
+                yield return StartCoroutine(TurnToFace(targetWaypoint));
             }
             yield return null; // "wait until the next frame" - yeld for one frame between each iteration of the while loop - it will make the movement smooth.
         }
+    }
+
+    // 3. Make guard rotate at each waypoint
+
+    IEnumerator TurnToFace(Vector3 lookTarget) {
+        Vector3 directionToLookTarget = (lookTarget - transform.position).normalized; // calculating the angle the guard will need to have on the Y axis to be facing the look target
+        float targetAngle = 90 - Mathf.Atan2(directionToLookTarget.z, directionToLookTarget.x) * Mathf.Rad2Deg; // Atan2 return angle in radians so wee need to user Rad2Deg
+        while (Mathf.DeltaAngle(transform.eulerAngles.y, targetAngle) > 0.05f)  { // while delta angle is greater than 0.05 keep rotating towards the target angle
+            float angle = Mathf.MoveTowardsAngle(transform.eulerAngles.y, targetAngle, turnSpeed * Time.deltaTime); //
+            transform.eulerAngles = Vector3.up * angle;
+            yield return null;
+        }
+
     }
 
     // 1. Draw spheres on each wayoint and connect them with lines
